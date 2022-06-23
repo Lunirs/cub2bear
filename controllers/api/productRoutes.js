2cb7bf0b7133b9b2e28a9fa53321c5c8334cabf4
Your Name <you@example.com>
2022-06-23 13:13:33 -0400

    added product route

---
+++ controllers/api/productRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Product, Categories } = require('../models');
+
+// The `/api/products` endpoint
+
+// get all products
+router.get('/', (req, res) => {
+  // find all products
+});
+
+
+
+module.exports = router;
