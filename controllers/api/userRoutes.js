f37cdccb25e9e503f44b3481a558ae3266f08e4e
Lunirs <dhong0925@gmail.com>
2022-06-23 15:11:17 -0400

    Added more keys to models and added relationships between them

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,77 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res.status(400).json({ message: 'Incorrect email or password' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect email or password' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.loggedIn = true;
+
+      res.json({ user: userData, message: 'You have logged in successfully.' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.delete('/user/:id', async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!userData) {
+      res
+        .status(404)
+        .json({ message: 'Could not find the user with this ID.' });
+      return;
+    }
+    res.json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
