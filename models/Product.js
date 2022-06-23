82bb808afb7c9e824b26f1f917283436377dd700
Your Name <you@example.com>
2022-06-23 19:14:26 -0400

    updated product model

--- models/Product.js
+++ models/Product.js
@@ -38,14 +38,6 @@ Product.init(
         key: 'id',
       },
     },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
   },
   {
     sequelize,
