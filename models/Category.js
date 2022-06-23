c4f433bf50da68cec595ae0ab8e25be79b11d0c5
Lunirs <dhong0925@gmail.com>
2022-06-23 13:15:35 -0400

    Added Category model

---
+++ models/Category.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
