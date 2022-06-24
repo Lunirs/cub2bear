39c78e549901c457953329095975ff359de57509
Lunirs <dhong0925@gmail.com>
2022-06-24 11:18:30 -0400

    changed up product model

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,9 @@
-// import our database connection from config.js 
 const sequelize = require('../config/connection');
-
-// import our important data of sequelize library 
 const { Model, DataTypes } = require('sequelize');
 
-// initialize product model table by extending off sequelizes model class 
 class Product extends Model {}
 
-// set up fields and rules for product model 
 Product.init(
-  // define columns
   {
     id: {
       type: DataTypes.INTEGER,
@@ -17,47 +11,48 @@ Product.init(
       autoIncrement: true,
       primaryKey: true,
     },
-
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-     },
-  
+    name: {
+      type: DataTypes.INTEGER,
+    },
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
     },
-
     description: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
-
     stock: {
-      stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 10,
-        validate: {
-        isNumeric: tru }
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
-
+    in_stock: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
     category_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
         model: 'category',
         key: 'id',
-      }
-    }
-  }, 
-
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "product",
+    modelName: 'product',
   }
 );
 
