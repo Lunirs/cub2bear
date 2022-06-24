52f0ff0d609d42351fe450f2bc97d80e701eabfb
Your Name <you@example.com>
2022-06-24 12:15:07 -0400

    delete a product

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -35,5 +35,24 @@ router.post('/', (req, res) => {
         });
 
 
+      // deleting products 
+      
+        router.delete('/:id', async (req, res) => {
+          // delete one product by its `id` value
+        try {
+          const productData = await Product.destroy({
+            where: {
+              id: req.params.id
+            }
+          });
+          if (!productData){
+            res.status(404).json({ message: "This product does not exist"});
+            return;
+          }
+          res.status(200).json(productData);
+        } catch (err) {
+          res.status(400).json(err);
+        }
+
 
 module.exports = router;
