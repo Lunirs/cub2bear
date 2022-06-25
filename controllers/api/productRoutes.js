c7fc23a66ac5b693de603197f467d01f0ef7a857
Taeyong Lee <d104601@hotmail.com>
2022-06-24 23:48:45 -0400

    finished basic of newProduct handler and pulbic js file

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -8,11 +8,17 @@ const withAuth = require('../../utils');
 
 router.post('/', withAuth, async (req, res) => {
   try {
+    const category = await Product.findOne({
+      where: {name: req.body.category}
+    });
+
+
     const newProductData = await Product.create({
-      product_name: req.body.product_name,
+      product_name: req.body.name,
       price: req.body.price,
       description: req.body.description,
-      category_id: req.body.category_id,
+      stock: req.body.stock,
+      category_id: category.category_id,
       user_id: req.session.user_id,
     });
     res.status(200).json(newProductData);
--- public/js/newProduct.js
+++ public/js/newProduct.js
@@ -1,11 +1,42 @@
 async function productFormHandler(event) {
     try {
         event.preventDefault();
+        const category = document.querySelector('#category').value.trim();
         const name = document.querySelector('#name').value.trim();
         const price = document.querySelector('#price').value.trim();
         const description = document.querySelector('#description').value.trim();
         const stock = document.querySelector('#stock').value.trim();
 
+        if(!name) {
+            alert("Please enter product name.");
+        }
+        else if(!price) {
+            alert("Please enter price");
+        }
+        else if(!description) {
+            alert("Please enter description.");
+        }
+        else {
+          const response = await fetch('/api/products', {
+            method: 'post',
+            body: JSON.stringify({
+                category,
+                name,
+                price,
+                description,
+                stock
+            }),
+            headers: { 'Content-Type': 'application/json' }
+          });
+      
+          if (response.ok) {
+            document.location.replace('/');
+          } else if(response.status == 400) {
+            alert(response.json.message);
+          } else {
+            alert(response.statusText);
+          }
+        }
     } catch (err) {
         res.status(500).json(err);
     }
